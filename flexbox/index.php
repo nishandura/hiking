0897ffc505a3204d6fd65f97494d5232d29780e1
nishandura <nishandura08@gmail.com>
2022-08-23 12:06:14 +0545

    added

---
+++ flexbox/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section>
+        <div class="nav">
+            <a href="pages/home.html">HOME</a>
+            <a href="pages/about.html">ABOUT US</a>
+            <a href="pages/contact.html">CONTACT US</a>
+        </div>
+    </section>
+    <section>
+        <div class="outcontainer">
+            <div class="innercontainer"><img src="uploads/image1.jpg" alt=""></div>
+            <div  class="innercontainer"><img src="uploads/image2.jpg" alt=""></div>
+            <div  class="innercontainer"><img src="uploads/image3.jpg" alt=""></div>
+        </div>
+    </section>
+    <section>
+        <h1>Hiking</h1>
+        <div>
+            <p>Hiking is simply defined as walking as recreational purposes for couple of days which are likely in the
+                hilly or mountain region. There are many adventure activities such as trekking, hiking, climbing,
+                rafting, safaris, scuba diving, skiing, paragliding, hunting, and racing etc but hiking is a natural
+                exercise for physical fitness among them, requires no special equipment. In Hiking, the most important
+                thing to remember is “The journey is more important than the destination”. When you think about Hiking,
+                you most think of beautiful nature, landscapes, forests, rivers and walking there to enjoy natural
+                scenery. Hiking is the great option an escape back to nature and a great way to forget hassles of the
+                city life.
+
+                Don’t consume alcohol during the treatment with Soma. Adverse effects of this drug are sleepiness and
+                drowsiness.
+
+                Hiking are likely to be a short (average walk of couple of hours in the park) or it could be a two weeks
+                to a month in the mountain range, it is depending on the hiking conditions and hike length. Trekking,
+                Bush-walking, Mountaineering are seemed different activities but it will not be false to say these are
+                the long disciplines or variations of Hiking.
+
+                Dhampus Hiking
+
+                Nepal, a country of abode Himalaya, is one of the finest travel destinations in the world for outdoor
+                activities such as Mountaineering and Peak Climbing, Hiking in Nepal, White Water Rafting, Jungle Safari
+                Tour, Trekking in Nepal etc. These all activities provide different adventure experience in life. Hiking
+                in Nepal is simple way to observe nature beauties, spiritual and unique cultures deserved by various
+                ethnic groups, century’s old temples and monasteries, historical places, World Heritage Sites and high
+                snow-capped mountains of Nepal. Hiking in Nepal also comprised to explore fine arts, old nice settlement
+                villages on 4-5 hours walking in the hilly or mountain region.
+
+                Nagarkot, Chisopani, Balthali and Sarangkot are the most visited and famous hiking destination in Nepal.
+                These places lie in the different region; significantly represent real taste and magnifying overview of
+                Nepal.
+
+                Nagarkot Hiking: Naturally rich site is located 32 km away from Kathmandu in the east, famous place for
+                sunrise and sunset view with panoramic mountain range. Beside sunrise, sunset and panoramic mountain
+                range; exploring old villages and Changu Narayan Temple (World Heritage Site – built in 17th century)
+                are the strong features of Nagarkot Hiking.
+
+                Chisopani Hiking: The best option for those who are seeking for little adventure trekking and a real
+                taste of hiking, offers an incredible view of spectacular landscape and mountain range including Mount
+                Everest. It is worth to be at Chisopani passing through villages, farming land and dense forest,
+                although it is tough climb to reach there.
+
+                Balthali Hiking: Balthali, old Newar village nearby Panauti (another Newar village) and Namo Buddha
+                (Buddhist monastery) is another good hike spot takes you to insight of Newari culture and fine arts.
+
+                Sarangkot Hiking: Sarangkot is situated on the north of Pokhara Valley, a famous spot for sunrise and
+                breath-taking views of Dhaulagiri and Annapurna range including Mount Dhaulagiri (8167m), Mount
+                Annapurna I (8091m, Machhapuchhre (a holy mountain 6997m) – a native name for Mount Fishtail and Mount
+                Manaslu (8163m). Sarangkot, an incredible hiking place, is not only famous for foreigner. It is also
+                famous attraction for domestic tourists and all nature lovers.</p>
+        </div>
+    </section>
+   <section>
+       <div class="foot">
+        <footer>
+        <p>contact: 9803459812</p>
+        <br>
+        <p>email: ram123@gmail.com</p>
+        </footer>
+       </div>
+   </section>
+</body>
+</html>
